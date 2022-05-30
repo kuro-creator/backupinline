6f26fe5fbce3aa8f1dc5cf9b7ec21514129c2d73
nyobabot4 <101924094+nyobabot4@users.noreply.github.com>
2022-05-30 13:07:14 +0700

    Update info.py

--- info.py
+++ info.py
@@ -36,5 +36,5 @@ Here you can search files in inline mode. Just press following buttons and start
 """
 
 START_MSG = environ.get('START_MSG', default_start_msg)
-SHARE_BUTTON_TEXT = 'Checkout {username} for searching files'
+SHARE_BUTTON_TEXT = 'Nonton anime dengan {username}\n\nUntuk mendapatkan akses hubungi @zxcxzcx'
 INVITE_MSG = environ.get('INVITE_MSG', 'Please join @.... to use this bot')
